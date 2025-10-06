830221c457f34c0ba4a9a47ebc20601840729489
Kauan <kauaanmatheus@gmail.com>
2025-10-06 18:17:40 -0300

    feat: Dropdown de opções de reservar pedido

--- src/components/botarReservarPedido/BotaoReservarPedido.tsx
+++ src/components/botarReservarPedido/BotaoReservarPedido.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { colors } from "../../utils/Constants";
 
@@ -7,10 +7,47 @@ const BotaoReservarPedidoContainer = styled.div`
     padding: 1rem;
     border-radius: 50%;
     color: white;
+    position: relative;
+`;
+
+const DropdownOpcoesReserva = styled.div`
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    width: 10rem;
+    position: absolute;
+    background-color: white;
+    left: -8.5rem;
+    top: 2.8rem;
+    border: 1px solid #ccc;
+    border-radius: 1rem;
+`;
+
+const PrimeiraOpcaoReserva = styled.div`
+    padding: .3rem .6rem .3rem .6rem;
+    color: black;
+`;
+
+const OpcaoReserva = styled.div`
+    padding: .3rem .6rem .3rem .6rem;
+    border-top: 1px solid #ccc;
+    color: black;
 `;
 
 export default function BotaoReservarPedido() {
-    return <BotaoReservarPedidoContainer>
-        <span>↓</span>
-    </BotaoReservarPedidoContainer>
+    const [dropDownOpcoesReservaVisivel, setDropDownOpcoesReservaVisivel] = useState<boolean>(false);
+
+    return <BotaoReservarPedidoContainer onClick={() => setDropDownOpcoesReservaVisivel(!dropDownOpcoesReservaVisivel)}>
+            {
+                dropDownOpcoesReservaVisivel
+                    ?
+                    <DropdownOpcoesReserva>
+                        <PrimeiraOpcaoReserva>Vou mandar um PIX</PrimeiraOpcaoReserva>
+                        <OpcaoReserva>Vou levar na festa</OpcaoReserva>
+                        <OpcaoReserva>Quero mandar entregar</OpcaoReserva>
+                    </DropdownOpcoesReserva>
+                    : null
+            }
+            <span>↓</span>
+        </BotaoReservarPedidoContainer>
 }
